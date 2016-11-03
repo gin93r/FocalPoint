4b994c84c12bfaa14ed3de33fc0f2c57a72327f6
Carl Morlock <cmorlock@embarkdigital.com>
2016-11-03 14:08:54 -0400

    Fixes #1.

--- src/FocalPoint.js
+++ src/FocalPoint.js
@@ -9,6 +9,20 @@
     // plugin definition
     $.fn.FocalPoint = function(options)
     {
+
+        Math.hypot = Math.hypot || function() {
+          var y = 0;
+          var length = arguments.length;
+
+          for (var i = 0; i < length; i++) {
+            if (arguments[i] === Infinity || arguments[i] === -Infinity) {
+              return Infinity;
+            }
+            y += arguments[i] * arguments[i];
+          }
+          return Math.sqrt(y);
+        };
+
         function calculatePosition(cont, img, p2)
         {
             // get the width and height of the container
